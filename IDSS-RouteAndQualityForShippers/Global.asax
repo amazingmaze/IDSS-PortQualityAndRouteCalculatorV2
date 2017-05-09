57b63a1410edf868acc7228ccdf20acd13dc83df
Mattias Hjalmarsson <maze.89@hotmail.com>
2017-05-09 09:35:50 +0200

    Add project files.

---
+++ IDSS-RouteAndQualityForShippers/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IDSS_RouteAndQualityForShippers.MvcApplication" Language="C#" %>
---
+++ IDSS-RouteAndQualityForShippers/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IDSS_RouteAndQualityForShippers.Startup))]
+namespace IDSS_RouteAndQualityForShippers
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
